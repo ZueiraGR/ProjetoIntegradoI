82069b22a90b61532a1221a3b4a348bcac2fcd0f
Marcos Antonio Vieira <marcosantonio3232@gmail.com>
2017-03-19 21:52:26 -0300

    Projeto inicial com pagina inicial incompleta.

---
+++ SistemaDeReservas/web/js/slide.js
@@ -0,0 +1,13 @@
+ $('.carousel.carousel-slider').carousel({fullWidth: true});
+ 
+$(".dropdown-button").dropdown();
+
+$(document).ready(function(){
+    $('ul.tabs').tabs();
+  });
+  
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
+
