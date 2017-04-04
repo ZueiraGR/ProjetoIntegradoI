08a72d11e48edc4f107797bc47e089f0a01efdbc
Marcos Antonio Vieira <marcosantonio3232@gmail.com>
2017-04-04 14:30:09 -0300

    Melhorias da interface gráfica.

--- SistemaDeReservas/build/web/js/init.js
+++ SistemaDeReservas/build/web/js/init.js
@@ -1,7 +1,21 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-
-  }); // end of document ready
+(function ($) {
+    $(function () {
+        $('.button-collapse').sideNav();
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+//Iniciar dropdow
+$(".dropdown-button").dropdown();
+
+//inicia tabs da janela
+$(document).ready(function () {
+    $('ul.tabs').tabs();
+});
+
+//inicia janelas modais
+$(document).ready(function () {
+    $('.modal').modal();
+});
+$(document).ready(function () {
+    $('.modal5').modal();
+});
--- SistemaDeReservas/web/js/slide.js
+++ SistemaDeReservas/web/js/slide.js
@@ -1,13 +1,1 @@
- $('.carousel.carousel-slider').carousel({fullWidth: true});
- 
-$(".dropdown-button").dropdown();
-
-$(document).ready(function(){
-    $('ul.tabs').tabs();
-  });
-  
-$(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal').modal();
-  });
-
+$('.carousel.carousel-slider').carousel({fullWidth: true});
