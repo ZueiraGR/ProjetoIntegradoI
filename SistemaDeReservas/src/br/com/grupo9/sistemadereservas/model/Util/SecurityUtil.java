07f9535969b90ad9215d83ed8f1d3bf814886aec
Marcos Vieira <marcosantoniovieiradesa@gmail.com>
2017-05-14 23:59:50 -0300

    Classe que irá gerar hash sha-256

---
+++ SistemaDeReservas/src/br/com/grupo9/sistemadereservas/model/Util/SecurityUtil.java
@@ -0,0 +1,30 @@
+package br.com.grupo9.sistemadereservas.model.Util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SecurityUtil {
+
+	public static String getHash(String texto) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			md.update(texto.getBytes());
+			System.out.println(md.toString());
+			return stringHexa(md.digest());
+		} catch (NoSuchAlgorithmException e) {
+			return null;
+		}
+	}
+	
+	private static String stringHexa(byte[] bytes) {
+		   StringBuilder s = new StringBuilder();
+		   for (int i = 0; i < bytes.length; i++) {
+		       int parteAlta = ((bytes[i] >> 4) & 0xf) << 4;
+		       int parteBaixa = bytes[i] & 0xf;
+		       if (parteAlta == 0) s.append('0');
+		       s.append(Integer.toHexString(parteAlta | parteBaixa));
+		   }
+		   return s.toString();
+		}
+
+}
