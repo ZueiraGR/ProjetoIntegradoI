873ae69e65851452b3dab1e341ee284217bda3be
Marcos Vieira <marcosantoniovieiradesa@gmail.com>
2017-05-15 00:05:22 -0300

    Classes e páginas que serão utilizadas para autenticação.

---
+++ SistemaDeReservas/src/br/com/grupo9/sistemadereservas/controle/servlet/Sair.java
@@ -0,0 +1,40 @@
+package br.com.grupo9.sistemadereservas.controle.servlet;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Sair
+ */
+@WebServlet("/sair.do")
+public class Sair extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Sair() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession sessao = request.getSession();
+		Date data = new Date();
+		System.out.println(data.getTime()+"Limpando a sessao: "+sessao.getId());
+		sessao.invalidate();
+		response.sendRedirect("");
+	}
+
+}
