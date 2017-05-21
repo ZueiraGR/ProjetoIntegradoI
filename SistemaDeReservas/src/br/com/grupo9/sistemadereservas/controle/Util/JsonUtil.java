7187d0184900afccc1d2ae7978ce2bc1693b45c1
Marcos Vieira <marcosantoniovieiradesa@gmail.com>
2017-05-21 20:44:42 -0300

    Classe responsável por receber o arquivo json e converter em objeto.

---
+++ SistemaDeReservas/src/br/com/grupo9/sistemadereservas/controle/Util/JsonUtil.java
@@ -0,0 +1,24 @@
+package br.com.grupo9.sistemadereservas.controle.Util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.google.gson.Gson;
+
+import br.com.grupo9.sistemadereservas.model.PO.UsuarioPO;
+
+public class JsonUtil {
+	
+	public static UsuarioPO converterJsonEmUsuario(HttpServletRequest request) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
+        String json = "";
+        if(br != null){
+            json = br.readLine();
+        }
+        Gson gson = new Gson();
+        return gson.fromJson(json, UsuarioPO.class);
+	}
+}
