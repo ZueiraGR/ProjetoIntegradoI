288b4288ba138ed34e22e5701aea95f65667f34d
Marcos Antonio Vieira <marcosantonio3232@gmail.com>
2017-05-23 13:11:31 -0300

    Ajustes do layout e design da tela de login.

--- SistemaDeReservas/WebContent/js/scriptsLogin.js
+++ SistemaDeReservas/WebContent/js/scriptsLogin.js
@@ -2,23 +2,25 @@ var LOGIN_INVALIDO= "Login inválido.";
 var SENHA_INCORRETA = "Senha Incorreta";
 
 $('#formularioDeLogin').submit(function (event) {
-	//event.preventDefault();
+	$("#barraCarregando").removeClass("hiddendiv");
 	var form = {"login":$("#login").val(),"senha":$("#senhaL").val()};
 	var formData = JSON.stringify(form);
     $.ajax({
         url: "autenticar.do",
         type: 'POST',
         data: formData,
-        progress: function(){
-        	
-        },
         success: function (data) {
+        	$("#retornoServidor").addClass("hiddendiv");
             if(data.loginValido==0 && data.senhaValida==0){
-	            alert(LOGIN_INVALIDO);
+            	$("#barraCarregando").addClass("hiddendiv");
+            	$("#mensagemRetorno").html(LOGIN_INVALIDO);
+            	$("#retornoServidor").removeClass("hiddendiv");
             }else if(data.loginValido==1 && data.senhaValida==0){
-            	alert(SENHA_INCORRETA);
+            	$("#barraCarregando").addClass("hiddendiv");
+            	$("#mensagemRetorno").html(SENHA_INCORRETA);
+            	$("#retornoServidor").removeClass("hiddendiv");
             }else if(data.loginValido==1 && data.senhaValida==1){
-            	setTimeout(entrar,3000);
+            	setTimeout(entrar,2000);
             }
         },
         cache: false,
