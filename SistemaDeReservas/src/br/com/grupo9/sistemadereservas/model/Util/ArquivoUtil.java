cf02a0d4af7aa944baecbfdc6f2f2181fd7c3b84
Marcos Vieira <marcosantoniovieiradesa@gmail.com>
2017-06-12 00:57:11 -0300

    Classe responsável por: -receber arquivo codificado em Base64 e salva-lo no servidor -Excluir arquivo quando necessário

---
+++ SistemaDeReservas/src/br/com/grupo9/sistemadereservas/model/Util/ArquivoUtil.java
@@ -0,0 +1,35 @@
+package br.com.grupo9.sistemadereservas.model.Util;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class ArquivoUtil {
+	
+	public static boolean salvar(String arquivoBase64, String nomeDoArquivo, String path){
+		try {
+			String base64ArquivoSemPropert = arquivoBase64.split(",")[1];
+			byte[] arquivoBytes = org.jboss.resteasy.util.Base64.decode(base64ArquivoSemPropert);
+			File arquivo = new File(path + nomeDoArquivo);
+			FileOutputStream salvar = new FileOutputStream(arquivo);
+			salvar.write(arquivoBytes);
+			salvar.flush();
+			salvar.close();
+			System.out.println("Arquivo salvado: " + nomeDoArquivo);
+			System.out.println("Local: " + path);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public static boolean excluir(String nomeDoArquivo, String path){
+		File arquivo = new File(path + nomeDoArquivo);
+		if (arquivo.exists()) {
+			arquivo.delete();
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
