c4c04d645ba8c9c7896dfdf305f64233aa9f6cba
Marcos Vieira <marcosantoniovieiradesa@gmail.com>
2017-06-17 14:09:23 -0300

    Ajuste na interface para permitir filtrar dados na listagem.

--- SistemaDeReservas/src/br/com/grupo9/sistemadereservas/interfaces/DAO.java
+++ SistemaDeReservas/src/br/com/grupo9/sistemadereservas/interfaces/DAO.java
@@ -9,7 +9,7 @@ public interface DAO<ClassePO> {
 	public boolean cadastrar(ClassePO entidade);
 	public ClassePO capturarPorId(ClassePO entidade);
 	public boolean atualizar(ClassePO entidade);
-	public List<ClassePO> listar(Integer pagina, Integer qtdRegistros);
+	public List<ClassePO> listar(Integer pagina, Integer qtdRegistros, String filtro);
 	public boolean excluir(ClassePO entidade);
 	public void fecharManager();
 	
