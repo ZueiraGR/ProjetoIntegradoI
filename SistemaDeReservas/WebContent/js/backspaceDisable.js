7c8bd69494698000764ed17f4d0ea6b801fed8b9
SadrackX <thallyelsom@hotmail.com>
2017-06-24 16:38:15 -0300

    possivel correção do script do backspace

--- SistemaDeReservas/WebContent/js/backspaceDisable.js
+++ SistemaDeReservas/WebContent/js/backspaceDisable.js
@@ -2,8 +2,7 @@ $(document).unbind('keydown').bind('keydown', function (event) {
     var doPrevent = false;
     if (event.keyCode === 8) {
         var d = event.srcElement || event.target;
-        if ((d.tagName.toUpperCase() === 'INPUT' && (d.type.toUpperCase() === 'TEXT' || d.type.toUpperCase() === 'PASSWORD' || d.type.toUpperCase() === 'FILE')) 
-             || d.tagName.toUpperCase() === 'TEXTAREA') {
+        if ((d.tagName.toUpperCase() === 'INPUT')) {
             doPrevent = false;
         }
         else {
