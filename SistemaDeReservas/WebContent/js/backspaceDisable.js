5b7aab7d02c4a4dcaab8a126be93abb53816a3b8
Marcos Vieira <marcosantoniovieiradesa@gmail.com>
2017-07-04 01:27:29 -0300

    Corrigir bug que não permitia apagar os caracteres em certos campos.

--- SistemaDeReservas/WebContent/js/backspaceDisable.js
+++ SistemaDeReservas/WebContent/js/backspaceDisable.js
@@ -2,7 +2,8 @@ $(document).unbind('keydown').bind('keydown', function (event) {
     var doPrevent = false;
     if (event.keyCode === 8) {
         var d = event.srcElement || event.target;
-        if ((d.tagName.toUpperCase() === 'INPUT')) {
+        if ((d.tagName.toUpperCase() === 'INPUT' && (d.type.toUpperCase() === 'TEXT' || d.type.toUpperCase() === 'PASSWORD' || d.type.toUpperCase() === 'FILE' || d.type.toUpperCase() === 'NUMBER' || d.type.toUpperCase() === 'EMAIL')) 
+             || d.tagName.toUpperCase() === 'TEXTAREA') {
             doPrevent = false;
         }
         else {
